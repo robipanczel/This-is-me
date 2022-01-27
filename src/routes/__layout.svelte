4a0488e3dad72797dd7587217ec083db93410427
Róbert Pánczél <robi.panczel@gmail.com>
2022-01-28 00:22:03 +0100

    Custom-ish color palette

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -14,27 +14,43 @@
 	$: isMobileView = screenWidth < 768 ? userMenuToggle : true;
 </script>
 
-<div class="flex flex-col min-h-screen bg-gray-500" bind:clientWidth={screenWidth}>
-	<div class="flex flex-row">
+<div class="flex flex-col min-h-screen bg-black" bind:clientWidth={screenWidth}>
+	<div class="flex flex-row flex-1">
 		{#if isMobileView}
 			<div
 				transition:fade
 				class="{isMobileView
 					? 'top-0'
-					: '-top-60'} md:relative absolute md:w-64 w-screen md:h-auto h-24 bg-blue-700"
+					: '-top-60'} md:relative absolute md:w-64 w-screen md:h-auto h-24 menu-bg"
 			>
 				<Header cssClass="" />
 			</div>
 		{/if}
 
-		<main class="flex-1 md:w-40 bg-green-700">
+		<main class="flex-1 md:w-40 body-bg">
 			<slot />
 		</main>
 	</div>
 
-	<Footer cssClass="h-24 bg-red-700" />
+	<div class="h-24 footer-bg">
+		<Footer cssClass="" />
+	</div>
 
 	<button class="absolute md:hidden block bottom-10 right-10" on:click|self={toggleMenu}>
 		X
 	</button>
 </div>
+
+<style>
+	.body-bg {
+		background-color: #e6e6e9;
+	}
+
+	.menu-bg {
+		background-color: #9999a1;
+	}
+
+	.footer-bg {
+		background-color: #66666e;
+	}
+</style>
